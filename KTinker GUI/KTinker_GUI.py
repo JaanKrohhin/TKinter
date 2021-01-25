d94a7b1df2d49f66887a7215e662d6041d95a820
Jaan Krohhin <jaan.krohin@gmail.com>
2021-01-25 20:27:07 +0200

    Made finishing touches, gave up on displaying the graph as an image

--- KTinker GUI/KTinker_GUI.py
+++ KTinker GUI/KTinker_GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,10 +7,8 @@ from math import sqrt
 from random import choice
 import os
 colours=["BlueViolet","Brown","CadetBlue","Crimson","ForestGreen","Gold","Sienna","Violet","Black","RoyalBlue"]
+rng=choice(colours)
 def roots(a,b,c):
-    a=int(a)
-    b=int(b)
-    c=int(c)
     D=b**2-4*a*c
     with open("variables.txt","w") as file:
         file.write(f"D={b}**2-4*{a}*{c}\n")
@@ -28,65 +27,81 @@ def roots(a,b,c):
             x=False
         return x
 def solve(event):
-    a=enta.get()
-    b=entb.get()
-    c=entc.get()
-    answer=roots(a,b,c)
-    if type(answer)==list:
-        if btn1.winfo_ismapped()==0:
-            btn1.grid(column=0,row=4)
-    lines=""
-    with open("variables.txt","r") as f:
-        if lbl.winfo_ismapped()==False:
-            lbl_grp.forget()
-            lbl.grid(column=1,row=0,rowspan=4)
-        for i in f.readlines():
-            lines+=i.strip("{}()")
-        lbl.configure(text=lines,justify=LEFT)
+    friend=True
+    try:
+        a=float(enta.get())
+    except:
+        TypeError
+        messagebox.showinfo("Error","'a' is not a valid number")
+        friend=False
+    try:
+        b=float(entb.get())
+    except:
+        TypeError
+        messagebox.showinfo("Error","'b' is not a valid number")
+        friend=False
+    try:
+        c=float(entc.get())
+    except:
+        TypeError
+        messagebox.showinfo("Error","'c' is not a valid number")
+        friend=False
+    if friend==False:
+        pass
+    else:
+        answer=roots(a,b,c)
+        if type(answer)==list:
+            if btn1.winfo_ismapped()==0:
+                btn1.grid(column=0,columnspan=2,row=4)
+        elif type(answer)!=list:
+            if btn1.winfo_ismapped()==1:
+                btn1.grid_forget()
+        lines=""
+        with open("variables.txt","r") as f:
+            for i in f.readlines():
+                lines+=i.strip("{}()")
+            lbl.configure(text=lines,justify=LEFT)
 def graph(event):
-    lbl.forget()
-    a=int(enta.get())
-    b=int(entb.get())
-    c=int(entc.get())
+    a=float(enta.get())
+    b=float(entb.get())
+    c=float(entc.get())
     answer=roots(a,b,c)
     if len(answer)==2:
+        answer.sort()
         x=np.arange(answer[0],answer[1]+0.01,0.01)
     elif len(answer)==1:
-        x=answer[0]
+        x=np.array(answer[0])
     y=a*x**2+b*x+c
-    plt.plot(x,y,color=f"{choice(colors)})",lw=3)
+    plt.xticks(np.arange(-15,16,3))
+    plt.yticks(np.arange(-15,16,3))
+    plt.grid(axis="both",c="black",lw=2,alpha=0.5)
+    plt.grid(True)
+    plt.plot(x,y,color=f"{rng}",lw=3)
     plt.show()
-    if os.path.exists("graph.png"):
-        os.remove("graph.png")
-    fig.savefig("graph.png")
-    img=PhotoImage(file=("graph.png"))
-    lbl.configure(image=img)
-
 scr=Tk()
 scr.title("Square Equation")
-scr.geometry("800x600")
-enta=Entry(scr,width=15,fg="Crimson",font="Ariel 14",bd=5)
-entb=Entry(scr,width=15,fg="Crimson",font="Ariel 14",bd=5)
-entc=Entry(scr,width=15,fg="Crimson",font="Ariel 14",bd=5)
+scr.geometry("600x300")
+enta=Entry(scr,width=15,fg="Orange",font="Ariel 14",bd=5)
+entb=Entry(scr,width=15,fg="Orange",font="Ariel 14",bd=5)
+entc=Entry(scr,width=15,fg="Orange",font="Ariel 14",bd=5)
 
-btn=Button(scr,width=15,text="Solve",font="Ariel 14",bd=5)
-btn1=Button(scr,width=15,text="Build Graph",font="Ariel 14",bd=5)
+btn=Button(scr,width=20,text="Solve",font="Ariel 14",bd=5)
+btn1=Button(scr,width=20,text="Graph",font="Ariel 14",bd=5)
 
-lbl=Label(scr,width=34,height=7,bg="gray",fg="Crimson",bd=5,anchor=NW,text="The solution will be posted here",font="Ariel 14",justify=CENTER)
-lbl_grp=Label(scr)
-
+lbl=Label(scr,width=34,height=7,bg="LightGray",fg="Orange",bd=5,anchor=NW,text="The solution will be posted here",font="Ariel 14",justify=CENTER)
+lbla=Label(scr,width=5,bg="LightGray",fg="Orange",bd=5,anchor=NW,text="a=",font="Ariel 14",justify=CENTER)
+lblb=Label(scr,width=5,bg="LightGray",fg="Orange",bd=5,anchor=NW,text="b=",font="Ariel 14",justify=CENTER)
+lblc=Label(scr,width=5,bg="LightGray",fg="Orange",bd=5,anchor=NW,text="c=",font="Ariel 14",justify=CENTER)
 btn1.bind("<Button-1>",graph)
 btn.bind("<Button-1>",solve)
 
-enta.grid(column=0,row=0)
-entb.grid(column=0,row=1)
-entc.grid(column=0,row=2)
-btn.grid(column=0,row=3)
-lbl.grid(column=1,row=0,rowspan=4)
+enta.grid(column=1,row=0)
+entb.grid(column=1,row=1)
+entc.grid(column=1,row=2)
+btn.grid(column=0,columnspan=2,row=3)
+lbl.grid(column=2,row=0,rowspan=4)
+lbla.grid(column=0,row=0)
+lblb.grid(column=0,row=1)
+lblc.grid(column=0,row=2)
 
 scr.mainloop()
-
-#import os
-#os.remove(filename)
-#fig.savefig("graph.png")
-#scr.iconbitmap()
